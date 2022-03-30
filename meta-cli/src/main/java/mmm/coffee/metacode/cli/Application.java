00c5d5dfff8a5310e07586bb3ec661655b02d589
Jon Caulfield <danesub@yahoo.com>
2022-03-30 09:41:41 -0500

    chore: bump metacode version number

--- meta-cli/src/main/java/mmm/coffee/metacode/cli/Application.java
+++ meta-cli/src/main/java/mmm/coffee/metacode/cli/Application.java
@@ -34,7 +34,7 @@ import picocli.CommandLine;
 @CommandLine.Command(
         name="metacode",
         description="A code generator",
-        version = "0.1.6",
+        version = "0.1.7",
         mixinStandardHelpOptions = true,
         subcommands = { GenerateCompletion.class, CreateCommand.class }
 
