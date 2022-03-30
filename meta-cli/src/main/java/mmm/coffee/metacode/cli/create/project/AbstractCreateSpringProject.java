49df34eb93f7ff4cc9e9fb888e0a1d49fcf51ac9
Jon Caulfield <danesub@yahoo.com>
2022-03-30 09:40:23 -0500

    build: exclude lombok-generated class from code coverage

---
+++ meta-cli/src/main/java/mmm/coffee/metacode/cli/create/project/AbstractCreateSpringProject.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2022 Jon Caulfield.
+ */   package mmm.coffee.metacode.cli.create.project;
+/**
+ * AbstractCreateSpringProject
+ */
+public class AbstractCreateSpringProject {
+}
