e734412514fd59d181e536586b0e1e155dc4effd
Jon Caulfield <danesub@yahoo.com>
2022-09-07 15:59:27 -0500

    chore: removed unused code

---
+++ meta-cli/src/test/java/mmm/coffee/metacode/cli/create/project/SubcommandCreateSpringBatchProjectTests.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2020 Jon Caulfield
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package mmm.coffee.metacode.cli.create.project;
+
+import mmm.coffee.metacode.cli.StringHelper;
+import mmm.coffee.metacode.common.ExitCodes;
+import mmm.coffee.metacode.common.descriptor.Descriptor;
+import mmm.coffee.metacode.common.descriptor.RestProjectDescriptor;
+import mmm.coffee.metacode.common.generator.ICodeGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErrAndOut;
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * Unit tests
+ */
+@SuppressWarnings("java:S5976") // S5976: don't refactor tests simply to satisfy sonarqube
+class SubcommandCreateSpringBootProjectTests {
+
+    protected CommandLine commandLine;
+    
+    /**
+     * Set up
+     */
+    @BeforeEach
+    public void setUp() {
+        /*
+         * NB: Since we're creating an instance of SubcommandCreateWebMvcProject,
+         * we're also imply that the first part of the command line,
+         * "create project spring-webmvc" has been entered. That's why the
+         * command line args in the tests start with the options of the spring-webmvc subcommand.
+         */
+        SubcommandCreateBootProject createProjectCommand = new SubcommandCreateBootProject(new FakeCodeGenerator());
+        commandLine = new CommandLine(createProjectCommand);
+    }
+
+    /**
+     * When all options on the command line are valid, execution should succeed
+     */
+    @Test
+    void shouldAcceptWellFormedCommandLine() {
+        String[] argv = StringHelper.toArgV("--name petstore --package acme.petstore --base-path /petstore");
+        int rc = commandLine.execute(argv);
+        assertThat(rc).isEqualTo(ExitCodes.OK);
+    }
+
+    @Test
+    void shouldErrorOnBadPackageName() throws Exception {
+        // Given: a invalid packageName
+        String[] argv = StringHelper.toArgV("--name petstore --package 123.#4$abc --base-path /petstore");
+        String errText = tapSystemErrAndOut(() -> {
+            int rc = commandLine.execute(argv);
+            assertThat(rc).isEqualTo(ExitCodes.INVALID_INPUT);
+        });
+    }
+
+    @Test
+    void shouldErrorIfNameIsMissing() throws Exception {
+        // Given: the required argument, --name, was not given
+        String[] argv = StringHelper.toArgV("--package acme.petstore --base-path /petstore");
+        String errText = tapSystemErrAndOut(() -> {
+            int rc = commandLine.execute(argv);
+            assertThat(rc).isEqualTo(ExitCodes.INVALID_INPUT);
+        });
+    }
+
+    @Test
+    void shouldErrorIfPackageIsMissing() throws Exception {
+        // Given: the required argument, --package, was not given
+        String[] argv = StringHelper.toArgV("--name petstore --base-path /petstore");
+        String errText = tapSystemErrAndOut(() -> {
+            int rc = commandLine.execute(argv);
+            assertThat(rc).isEqualTo(ExitCodes.INVALID_INPUT);
+        });
+    }
+
+    /**
+     * --base-path is not a required parameter, so leaving it off the command line should be accepted
+     */
+    @Test
+    void shouldAcceptMissingBasePathOption() {
+        String[] argv = StringHelper.toArgV("--name petstore --package acme.petstore");
+        int rc = commandLine.execute(argv);
+        assertThat(rc).isEqualTo(ExitCodes.OK);
+    }
+
+    /**
+     * Verify the --group option is recognized
+     */
+    @Test
+    void shouldAcceptGroupOption() {
+        String[] argv = StringHelper.toArgV("--name petstore --package acme.petstore --group org.acme.petstore");
+        int rc = commandLine.execute(argv);
+        assertThat(rc).isEqualTo(ExitCodes.OK);
+    }
+
+    @Test
+    void shouldAcceptPostgresSupportOption() {
+        String[] argv = StringHelper.toArgV("--name petstore --package acme.petstore --group org.acme.petstore --add postgres");
+        int rc = commandLine.execute(argv);
+        assertThat(rc).isEqualTo(ExitCodes.OK);
+    }
+
+    @Test
+    void shouldAcceptLiquibaseSupportOption() {
+        String[] argv = StringHelper.toArgV("--name petstore --package acme.petstore --group org.acme.petstore --add liquibase");
+        int rc = commandLine.execute(argv);
+        assertThat(rc).isEqualTo(ExitCodes.OK);
+    }
+
+    @Test
+    void shouldAcceptTestContainersSupportOption() {
+        String[] argv = StringHelper.toArgV("--name petstore --package acme.petstore --group org.acme.petstore --add testcontainers");
+        int rc = commandLine.execute(argv);
+        assertThat(rc).isEqualTo(ExitCodes.OK);
+    }
+
+    @Test
+    void shouldAcceptMultipleSupportOption() {
+        String[] argv = StringHelper.toArgV("--name petstore --package acme.petstore --group org.acme.petstore --add postgres testcontainers");
+        int rc = commandLine.execute(argv);
+        assertThat(rc).isEqualTo(ExitCodes.OK);
+    }
+
+    
+    /**
+     * A fake code generator suitable for testing
+     */
+
+    public static class FakeCodeGenerator implements ICodeGenerator<RestProjectDescriptor> {
+        public void setDescriptor(Descriptor ignored) {
+            // empty
+        }
+
+        /**
+         * Performs the code generation. Returns:
+         * 0 = success
+         * 1 = general error
+         *
+         * @return the exit code, with zero indicating success.
+         */
+        @Override
+        public int generateCode(RestProjectDescriptor ignored) {
+            return 0;
+        }
+
+        public FakeCodeGenerator doPreprocessing(RestProjectDescriptor ignored) { return this; }
+    }
+}
--- meta-common/src/main/java/mmm/coffee/metacode/common/stereotype/MetaTemplateModel.java
+++ meta-common/src/main/java/mmm/coffee/metacode/common/stereotype/MetaTemplateModel.java
@@ -15,11 +15,13 @@ import mmm.coffee.metacode.annotations.jacoco.Generated;
 public interface MetaTemplateModel {
     
     /*
-     * These are the valid values for the top level variable
-     * (returned by {@code getTopLevelVariable()}, below).
-     * Template's have variables that look like '${project.someField}'
-     * or '${endpoint.someField}'. The top-level variable, in template-speak,
-     * is that first token, 'project' or 'endpoint'.
+     * This code base passes a POJO to Freemarker templates
+     * as the approach to passing template variables. In the
+     * template variable names, we use property names that
+     * are either `${project.whateverPropertyName}` or `${endpoint.whateverPropertyName}`.
+     * The two `Key` values shown in this class are those two prefixes:
+     * `project` or `endpoint`. In template-speak, these are
+     * `top-level variables`, since they are the first token.
      *
      * This {@code Key} class serves as the source-of-record
      * for any top-level variables that may be defined in any
@@ -37,27 +39,6 @@ public interface MetaTemplateModel {
         }
         public String value() { return value; }
     }
-
-    /*
-     * These are the legal values for the 'framework'
-     * variable in the template models. Our current templates
-     * use expressions like "if (project.framework == 'WEBFLUX') ...";
-     * This {@code Framework} class serves as the source-of-record
-     * of the values used by templates.
-     */
-    @Generated
-    enum Framework {
-        WEBMVC ("WEBMVC"),
-        WEBFLUX ("WEBFLUX");
-
-        private final String value;
-        Framework(String value) { this.value = value; }
-        public String value() { return value; }
-
-        public final boolean isWebFlux() { return WEBFLUX.value.equals(this.value); }   // TODO: Does anything call this?
-        public final boolean isWebMvc() { return WEBMVC.value.equals(this.value); }     // TODO: Does anything call this?
-    }
-
 
     String getTopLevelVariable();
 }
